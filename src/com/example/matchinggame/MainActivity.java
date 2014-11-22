e4e14bad4a3f61329a81c0d77fef113e5d2e5a53
Henry <longh@bxscience.edu>
2014-11-21 20:35:18 -0500

    updated with previous chages
    ^^

--- src/com/example/matchinggame/MainActivity.java
+++ src/com/example/matchinggame/MainActivity.java
@@ -13,7 +13,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends Activity{
-
+private MatchGrid grid = new MatchGrid();
+private Button b00, b01, b02, b03, b10, b11, b12, b13, b20, b21, b22, b23, b30, b31, b32, b33;
 FragmentManager fragmentManager = getFragmentManager();
 	
 Fragment fragment = fragmentManager.findFragmentByTag("FragmentGrid");
@@ -42,6 +43,39 @@ Fragment fragment2 = fragmentManager.findFragmentByTag("fragmentStart");
                 	fragmentManager.beginTransaction().replace(R.id.FragmentGrid, fragment2).commit();    
             	}
 		});
+		
+		b00 = (Button) findViewById(R.id.b00);
+		b00.setTag(0,grid.getTile(0,0));
+		b01 = (Button) findViewById(R.id.b01);
+		b01.setTag(0,grid.getTile(0,1));
+		b02 = (Button) findViewById(R.id.b02);
+		b02.setTag(0,grid.getTile(0,2));
+		b03 = (Button) findViewById(R.id.b03);
+		b03.setTag(0,grid.getTile(0,3));
+		b10 = (Button) findViewById(R.id.b10);
+		b10.setTag(0,grid.getTile(1,0));
+		b11 = (Button) findViewById(R.id.b11);
+		b11.setTag(0,grid.getTile(1,1));
+		b12 = (Button) findViewById(R.id.b12);
+		b12.setTag(0,grid.getTile(1,2));
+		b13 = (Button) findViewById(R.id.b13);
+		b13.setTag(0,grid.getTile(1,3));
+		b20 = (Button) findViewById(R.id.b20);
+		b20.setTag(0,grid.getTile(2,0));
+		b21 = (Button) findViewById(R.id.b21);
+		b21.setTag(0,grid.getTile(2,1));
+		b22 = (Button) findViewById(R.id.b22);
+		b22.setTag(0,grid.getTile(2,2));
+		b23 = (Button) findViewById(R.id.b23);
+		b23.setTag(0,grid.getTile(2,3));
+		b30 = (Button) findViewById(R.id.b30);
+		b30.setTag(0,grid.getTile(3,0));
+		b31 = (Button) findViewById(R.id.b31);
+		b31.setTag(0,grid.getTile(3,1));
+		b32 = (Button) findViewById(R.id.b32);
+		b32.setTag(0,grid.getTile(3,2));
+		b33 = (Button) findViewById(R.id.b33);
+		b33.setTag(0,grid.getTile(3,3));
 	}
 	
 
